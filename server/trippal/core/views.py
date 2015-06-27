707acb484b0487272ccbead0cc60bd94d2237037
silentninja <mmukesh95@hotmail.com>
2015-06-28 01:34:05 +0530

    Dumped json data

--- server/trippal/core/views.py
+++ server/trippal/core/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
 from .models import Place, Location
+from django.core import serializers
+from django.http import HttpResponse
+import json
+import os
 # Create your views here.
 
 
 def get_locations(request):
     activity = request.GET.get('activity')
+    locations = Place.objects.filter(activites__icontains=activity)
+    return HttpResponse(serializers.serialize("json", locations))
+
+
+# def get_places(request):
+#     place = request.GET.get('id')
+#     locations = Place.objects.filter(activites__icontains=activity)
+#     return HttpResponse(serializers.serialize("json", locations))
