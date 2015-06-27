84b1f27f123e256c52dd655a5faf2fc1b898b9d2
silentninja <mmukesh95@hotmail.com>
2015-06-27 20:44:45 +0530

    Added production config

--- server/trippal/trippal/settings/production.py
+++ server/trippal/trippal/settings/production.py
@@ -1,1 +1,22 @@
+import os
 from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': os.environ['DATABASE_HOST'],
+        'PORT': '5432',
+    }
+}
+
+ALLOWED_HOSTS = ['*']
+
+BROKER_TRANSPORT = 'redis'
+REDIS_URL = 'redis://localhost:6379/0'
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+STATIC_ROOT = os.environ['STATIC_ROOT']
