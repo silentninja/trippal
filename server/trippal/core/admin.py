60ce1e94614487e9d447daeaec638d6dcaadb79c
Arvind Nedumaran <arvind@Arvinds-MacBook-Pro.local>
2015-06-28 03:14:12 +0530

    Added Places model

--- server/trippal/core/admin.py
+++ server/trippal/core/admin.py
@@ -1,5 +1,18 @@
-from django.contrib import admin
-from .models import Place, Location
+from django.contrib import admin 
+from .models import Place, Location, Activity
 # Register your models here.
-admin.site.register(Place)
-admin.site.register(Location)
+
+admin.site.register(Activity)
+
+class PlaceInline(admin.TabularInline):
+	model  = Place
+	fieldsets = (
+        (None, {
+            'fields': ('name', 'activities')
+        }),
+       )
+
+class LocationAdmin(admin.ModelAdmin):
+    inlines = [ PlaceInline, ]
+    
+admin.site.register(Location, LocationAdmin)
