eb16fd0beca7a8f730b58b21040e86d3d4139f76
Kumar Nielarshi <nielarshi@gmail.com>
2015-06-28 05:44:47 +0530

    Integrated map

--- client/trippal/www/js/controllers.js
+++ client/trippal/www/js/controllers.js
@@ -25,13 +25,89 @@ angular.module('travelchef.controllers', [])
       $scope.$on('$destroy', off);
 })
 
-.controller('PlacesCtrl', function($scope, $ionicModal, $timeout, $state, TripService, ActivityService) {
+.controller('PlacesCtrl', function($scope, $ionicModal, $timeout, $state, TripService, ActivityService, TripChefService, $timeout) {
   $scope.places = TripService.getPlaces();
   $scope.activity = ActivityService.getSelectedActivity();
+  
+  var Marker = function(id, latitude, longitude, labelContent) {
+      this.id = id;
+      this.coords = {
+        latitude : latitude,
+        longitude : longitude
+      },
+      this.options = {
+        labelContent : labelContent,
+        labelClass : "label-marker"
+      }
+  };
+
+  var Circle = function(id, latitude, longitude) {
+    this.id =  id,
+    this.center = {
+      latitude: latitude,
+      longitude: longitude
+    },
+    this.radius = 500,
+    this.stroke = {
+      color: '#08B21F',
+      weight: 2,
+      opacity: 1
+    },
+    this.fill = {
+      color: '#08B21F',
+      opacity: 0.5
+    }
+  };
+
+  var Line = function(id, to, from) {
+      this.id = id;
+      this.path = [];
+      this.path.push(to);
+      this.path.push(from);
+      this.stroke =  {
+        color: '#6060FB',
+        weight: 2
+      }
+  }
+
   $scope.choosePlace = function(place) {
     $scope.smaller = true;
     $scope.selectedPlace = place;
     TripService.setSelectedPlace(place);
+    TripChefService.getPlacesInOrder("attr1");
+    $scope.map = { center: {latitude: $scope.selectedPlace.latitude, longitude: $scope.selectedPlace.longitude }, zoom: 14 };
+    $scope.circles = [];
+    $scope.markers = [];
+    $scope.polylines = [];
+
+    if($scope.selectedPlace && $scope.selectedPlace.attractions && $scope.selectedPlace.attractions.length > 0) {
+      $scope.circles.push(new Circle(0, $scope.selectedPlace.attractions[0].lat, $scope.selectedPlace.attractions[0].lon));
+      $scope.selectedPlace.attractions.forEach(function(attraction) {
+        console.log(attraction);
+        $scope.markers.push(new Marker(attraction.id, attraction.lat, attraction.lon, attraction.name));
+      });
+      
+      $scope.preferredPath = [1, 0, 2];
+
+      $scope.selectedPlace.attractions.forEach(function(attraction, index) {
+        if((index+1) < $scope.preferredPath.length) {
+          var line = new Line(
+            index, 
+            { 
+              latitude : $scope.markers[index].coords.latitude, 
+              longitude : $scope.markers[index].coords.longitude 
+            }, 
+            { 
+              latitude : $scope.markers[index+1].coords.latitude, 
+              longitude : $scope.markers[index+1].coords.longitude 
+            }
+          );
+          $scope.polylines.push(line);
+        }  
+      });
+    }
+    
+
     $state.go("app.searchresult.place");
   };
 
--- client/trippal/www/js/services.js
+++ client/trippal/www/js/services.js
@@ -15,47 +15,92 @@ angular.module("travelchef.services", [])
 		this.reviews = reviews || [];
 		this.approxCost = approxCost || 1000;
 		this.similarity = similarity || 1;
-
 	};
 
-	var Event = function() {
-
+	var Attraction = function(id, name, lat, lon, categories, openTime, closeTime, visitingTime, suitableTime, ratings, reviews, approxCost, avgrating) {
+		this.id = id;
+		this.name = name;
+		this.lat = lat;
+		this.lon = lon;
+		this.categories = categories;
+		this.openTime = openTime || 8.30;
+		this.closeTime = closeTime || 5.30;
+		this.visitingtime = visitingTime || 2;
+		this.suitableTime = suitableTime || 2;
+		this.ratings = ratings || [];
+		this.reviews = reviews || [];
+		this.approxCost = approxCost || 400;
+		this.avgrating = avgrating || 3.5;
 	};
 
 	var initialPlaces = [
 		new Place(
-				"",
-		 		"", 
+				15.4989,
+		 		73.8278, 
 		 		"Goa", 
 		 		"", 
 		 		"Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 16th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to laid-back fishing villages such as Agonda.", 
 		 		"img/goa.jpg",
-		 		["Goa beaches", "Bar", "Restaurant"]
+		 		[
+		 			new Attraction(
+		 					"attr1",
+		 					"Goa Beach",
+		 					15.4989,
+		 					73.82,
+		 					[
+		 						"beach",
+		 						"bar",
+		 						"volleyball"
+		 					]
+		 			),
+		 			new Attraction(
+		 					"attr2",
+		 					"Goa Church",
+		 					15.4989,
+		 					73.8278,
+		 					[
+		 						"church",
+		 						"religious"
+		 					]
+		 			),
+		 			new Attraction(
+		 					"attr3",
+		 					"Angel Hack",
+		 					15.485,
+		 					73.81,
+		 					[
+		 						"hackathon",
+		 						"development"
+		 					]
+		 			)
+		 		]
 		 	),
 		new Place(
-				"", 
-				"", 
+				28.6139, 
+				77.2090, 
 				"Delhi", 
 				"", 
 				"Delhi (/ˈdɛli/, Hindustani pronunciation: [d̪ɪlliː] Dilli), officially the National Capital Territory of Delhi, is the Capital territory of India.[3] It has a population of about 11 million and a metropolitan population of about 16.3 million, making it the second most populous city and second most populous urban agglomeration in India.[2][4] Such is the nature of urban expansion in Delhi that its growth has expanded beyond the NCT to incorporate towns in neighbouring states and at its largest extent can count a population of about 25 million residents as of 2014.", 
 				"img/delhi.jpg",
-				["Red Fort", "New Delhi", "Restaurant"]
+				[]
 			),
 		new Place(
-				"", 
-				"", 
+				11.9310, 
+				79.7852, 
 				"Pondicherry", 
 				"", 
 				"Pondicherry (or Puducherry), a French colonial settlement in India until 1954, is now a Union Territory town bounded by the southeastern Tamil Nadu state. Its French legacy is preserved in its French Quarter, with tree-lined streets, mustard-colored colonial villas and chic boutiques. A seaside promenade runs along the Bay of Bengal and passes several statues, including a 4m-high Gandhi Memorial.", 
-				"img/pondy.jpg"
+				"img/pondy.jpg",
+				[]
 			),
 		new Place(
-				"", 
-				"", 
+				12.4208, 
+				75.7397, 
 				"Coorg", 
 				"", 
 				"Kodagu also known as Kodava Nadu, is an administrative district in Karnataka, India. It occupies an area of 4,102 square kilometres (1,584 sq mi) in the Western Ghats of southwestern Karnataka. In 2001 its population was 548,561, 13.74% of which resided in the district's urban centres, making it the least populous of the 30 districts in Karnataka.[3] The district is bordered by Dakshina Kannada district to the northwest, Hassan district to the north, Mysore district to the east, Kannur district of Kerala to the southwest, and the Wayanad district of Kerala to the south. Agriculture is the most important factor that upholds the economy of Kodagu and the main crops cultivated in this region are rice and coffee. Coorg is rich in natural resources which included timber and spices. Madikeri (English: Mercara) is the headquarters of Kodagu.", 
-				"img/coorg.png"
+				"img/coorg.png",
+				[]
 			)
 	];
 
@@ -97,5 +142,164 @@ angular.module("travelchef.services", [])
 		getInitialActivities : getInitialActivities,
 		setSelectedActivity : setSelectedActivity,
 		getSelectedActivity : getSelectedActivity
+	}
+})
+.factory('TripChefService', function(TripService, ActivityService) {
+
+	var assumptions = {
+		starttime : 8.30,
+		lunchtime : 12.30,
+		snackstime : 17.30,
+		dinner : 19.30,
+		endtime : 21,
+		threshold : 1
+	};
+
+	var Rating = {
+		REJECT : 0,
+		LEAST_FEASIBLE : 1,
+		LESS_FEASIBLE : 2,
+		LIKELY : 3,
+		HIGHLY_LIKELY : 4,
+		FAVOURABLE : 5
+	};
+
+	var AttractionRating = function(id, rating) {
+		this.id = id;
+		this.rating = rating;
+	};
+
+	var getVisitingTimeFromPlace = function(fromplace, attraction) {
+		//check traffic also
+		return 3;
+	};
+
+	var checkRatingOfPlaceBasedOnDuration = function(fromplace, starttime, endtime) {
+		
+		var attractionRatings = [];
+
+		var attractions = TripService.getSelectedPlace().attractions;
+
+		var duration = endtime - starttime;
+		
+		attractions.forEach(function(attraction) {
+
+			//distance based feasibility
+			var goingtime = getVisitingTimeFromPlace(fromplace, attraction);
+			var comingtime = goingtime;
+			var visitingtime = attraction.visitingTime;
+
+			var totalVisitingTime = (goingtime + visitingtime + comingtime);
+
+			
+			if(duration < goingtime) {
+				attractionRatings.push(new AttractionRating(attraction.id, Rating.REJECT));
+				return;
+			}
+			if(duration < goingtime + visitingtime) {
+				attractionRatings.push(new AttractionRating(attraction.id, Rating.REJECT));
+				return;
+			}
+			
+			if(duration < totalVisitingTime) {
+				if((duration - totalVisitingTime) < assumptions.threshold) {
+					attractionRatings.push(new AttractionRating(attraction.id, Rating.LIKELY));
+					return;
+				} else {
+					attractionRatings.push(new AttractionRating(attraction.id, Rating.LESS_FEASIBLE));
+					console.log("something may be missed");
+					return;
+				}
+			}
+			if(duration > goingtime) {
+				if(duration > (goingtime + visitingtime)) {
+					if(duration > totalVisitingTime) {
+						attractionRatings.push(new AttractionRating(attraction.id, Rating.FAVOURABLE));
+						return;
+					}
+				} else {
+					if((duration - totalVisitingTime) < assumptions.threshold) {
+						attractionRatings.push(new AttractionRating(attraction.id, Rating.HIGHLY_LIKELY));
+						return;
+					}
+				}
+			}
+
+			//relevance based feasibility
+			attractionRatings.push(new AttractionRating(attraction.id, Rating.LESS_FEASIBLE));
+			return;
+		});
+
+		return attractionRatings;
+	};
+
+	var checkRatingOfPlaceBasedOnRating = function() {
+		var attractions = TripService.getSelectedPlace().attractions;
+		var attractionRatings = [];
+		attractions.forEach(function(attraction) {
+			//rating based feasibility
+			var rating = attraction.avgrating;
+			attractionRatings.push(new AttractionRating(attraction.id, Math.floor(rating)));
+		});
+		return attractionRatings;
+	};
+
+	var checkRatingOfPlaceBasedOnRelevance = function() {
+		var attractions = TripService.getSelectedPlace().attractions;
+		var attractionRatings = [];
+		attractions.forEach(function(attraction) {
+			//rating based feasibility
+			var tags = attraction.categories;
+			var tagOfActivity = ActivityService.getSelectedActivity();
+
+			if(tags.indexOf(tagOfActivity) != -1) {
+				attractionRatings.push(new AttractionRating(attraction.id, Rating.FAVOURABLE));
+			} else {
+				attractionRatings.push(new AttractionRating(attraction.id, Rating.REJECT));
+			}
+		});
+		return attractionRatings;
+	};
+
+	var checkRatingOfPlaceBasedOnCostFactors = function(cost) {
+		var attractions = TripService.getSelectedPlace().attractions;
+		var attractionRatings = [];
+		attractions.forEach(function(attraction) {
+			//rating based feasibility
+			attractionRatings.push(new AttractionRating(attraction.id, Rating.HIGHLY_LIKELY));
+		});
+		return attractionRatings;
+	};
+
+	var getPlacesInOrder = function(fromplace, starttime, endtime, cost) {
+		var durationFeasibilityRatings = checkRatingOfPlaceBasedOnDuration(fromplace, (starttime || assumptions.starttime), (endtime || assumptions.endtime));
+		var relevanceFeasibilityRatings = checkRatingOfPlaceBasedOnRelevance();
+		var ratingFeasibilityRatings = checkRatingOfPlaceBasedOnRating();
+		var costFeasibilityRatings = checkRatingOfPlaceBasedOnCostFactors(cost || 3000);
+
+		var attractions = TripService.getSelectedPlace().attractions;
+
+		var similarityRatings = [];
+
+		var SimilarityRating = function(id, rating) {
+			this.id = id;
+			this.rating = rating;
+		};
+
+		attractions.forEach(function(attraction, index) {
+			var similarityRating = 0;
+			similarityRating += durationFeasibilityRatings[index].rating * 6;
+			similarityRating += relevanceFeasibilityRatings[index].rating * 5;
+			similarityRating += ratingFeasibilityRatings[index].rating * 4;
+			similarityRating += costFeasibilityRatings[index].rating * 4;
+			similarityRatings.push(new SimilarityRating(attraction.id, similarityRating / 19));
+		});
+
+		console.log(similarityRatings);
+		return similarityRatings;
+	};
+
+	return {
+		getPlacesInOrder : getPlacesInOrder
 	}
 });
